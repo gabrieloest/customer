bc86ad34951b9f92e5c3365cccd8d41490398745
Gabriel Oest <gabriel.oest@esig.com.br>
2018-10-05 15:26:24 -0300

    Fix the logger

--- src/main/java/com/backend/customer/resources/CustomerController.java
+++ src/main/java/com/backend/customer/resources/CustomerController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,29 +28,34 @@ import com.backend.customer.repository.CustomerRepository;
 @RequestMapping("/api")
 public class CustomerController {
 
+	private final static Logger logger = LoggerFactory.getLogger(CustomerController.class);
+
 	@Autowired
 	CustomerRepository repository;
 
 	@GetMapping("/customers")
 	public List<Customer> getAllCustomers() {
-		System.out.println("Get all Customers...");
+		logger.info("Get all Customers...");
 
 		List<Customer> customers = new ArrayList<>();
 		repository.findAll().forEach(customers::add);
+
+		logger.info(customers.size() + " Customers are found");
 
 		return customers.stream().sorted((c1, c2) -> c1.getName().compareTo(c2.getName())).collect(Collectors.toList());
 	}
 
 	@PostMapping(value = "/customers/create")
 	public Customer postCustomer(@RequestBody Customer customer) {
-
+		logger.info("Save " + customer.toString());
 		Customer _customer = repository.save(new Customer(customer.getName(), customer.getAge()));
+		logger.info("Saved with ID = " + _customer.getId() + "...");
 		return _customer;
 	}
 
 	@DeleteMapping("/customers/{id}")
 	public ResponseEntity<String> deleteCustomer(@PathVariable("id") long id) {
-		System.out.println("Delete Customer with ID = " + id + "...");
+		logger.info("Delete Customer with ID = " + id + "...");
 
 		repository.deleteById(id);
 
@@ -57,7 +64,7 @@ public class CustomerController {
 
 	@DeleteMapping("/customers/delete")
 	public ResponseEntity<String> deleteAllCustomers() {
-		System.out.println("Delete All Customers...");
+		logger.info("Delete All Customers...");
 
 		repository.deleteAll();
 
@@ -66,14 +73,15 @@ public class CustomerController {
 
 	@GetMapping(value = "customers/age/{age}")
 	public List<Customer> findByAge(@PathVariable int age) {
-
+		logger.info("Get all Customers by AGE = " + age + "...");
 		List<Customer> customers = repository.findByAge(age);
+		logger.info(customers.size() + " Customers are found");
 		return customers;
 	}
 
 	@PutMapping("/customers/{id}")
 	public ResponseEntity<Customer> updateCustomer(@PathVariable("id") long id, @RequestBody Customer customer) {
-		System.out.println("Update Customer with ID = " + id + "...");
+		logger.info("Update Customer with ID = " + id + "...");
 
 		Optional<Customer> customerData = repository.findById(id);
 
